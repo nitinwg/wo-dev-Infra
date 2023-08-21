144b85fa944112a7f4d2cdf982f00bf9c9cd4f0b
nitinwg <142773645+nitinwg@users.noreply.github.com>
2023-08-21 13:57:34 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "wd-tf-state-bucket"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table"
+    dynamodb_table = "wd-dynamo-db-table"
   }
 }
